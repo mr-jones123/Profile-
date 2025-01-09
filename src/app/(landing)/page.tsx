7cd203317871366727cc2550e203ae97343e89f6
mr-jones123 <xylacap@gmail.com>
2025-01-10 00:40:30 +0800

    feat: certification section modified

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import { PinContainer } from "@/components/ui/3d-pin";
 import {
   Card,
@@ -27,11 +28,11 @@ export default function Home() {
             I am xy.
           </h1>
           <div className="flex items-center flex-col md:items-start gap-5 ">
-            <h2 className="text-customGrey text-3xl md:text-5xl font-light leading-[1.2] tracking-[-0.015em] z-10">
-              The Tech Herald .
-            </h2>
+          <h2 className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 via-neutral-300 to-neutral-500 text-3xl md:text-5xl font-light leading-[1.2] tracking-[-0.015em] z-10 py-1">
+              Software Engineer.  
+          </h2>
             <p className="text-customGrey font-extralight text-sm md:text-md italic leading-normal z-10 text-center md:text-left">
-              Fun Fact: My favorite character is Viktor from Arcane! See why.
+              Evolving with technology. One iteration at a time.
             </p>
           </div>
           <BackgroundBeams />
@@ -104,64 +105,58 @@ export default function Home() {
         </motion.section>
 
         <motion.section
-          className="flex flex-col mt-40 items-center"
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-        >
-          <div className="flex flex-col items-center gap-4 md:gap-6 max-w-[800px] px-4 md:px-0">
-            <h1 className="text-customWhite text-3xl md:text-5xl font-bold text-center">
-              Certificates
-            </h1>
-            <h2 className="text-customGrey text-lg md:text-xl text-center">
-              Proof of my skills.
-            </h2>
-          </div>
+      className="mt-40 px-4 md:px-0"
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="text-center mb-12">
+        <h1 className="text-customWhite text-3xl md:text-5xl font-bold">Certifications</h1>
+        <h2 className="text-customGrey text-lg md:text-xl mt-2">Milestones in my journey</h2>
+      </div>
+      <motion.div
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
+        variants={{
+          hidden: { opacity: 0 },
+          show: {
+            opacity: 1,
+            transition: {
+              staggerChildren: 0.2,
+            },
+          },
+        }}
+        initial="hidden"
+        whileInView="show"
+      >
+        {certifications.map((cert, index) => (
           <motion.div
-            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 px-4 md:px-0"
+            key={index}
             variants={{
-              hidden: { opacity: 0 },
-              show: {
-                opacity: 1,
-                transition: {
-                  staggerChildren: 0.2,
-                },
-              },
+              hidden: { opacity: 0, y: 20 },
+              show: { opacity: 1, y: 0 },
             }}
-            initial="hidden"
-            whileInView="show"
           >
-            {certifications.map((certificate, key) => (
-              <motion.div
-                key={key}
-                className="w-72 mt-12"
-                variants={{
-                  hidden: { opacity: 0, y: 20 },
-                  show: { opacity: 1, y: 0 },
-                }}
-              >
-                <PinContainer
-                  href={certificate.link}
-                  title={certificate.title}
-                  containerClassName="h-72"
-                >
-                  <Card className="w-64 h-64">
-                    <CardHeader className="p-4">
-                      <CardTitle className="text-xl md:text-3xl tracking-tight">
-                        {certificate.title}
-                      </CardTitle>
-                    </CardHeader>
-                    <CardContent className="p-4">
-                      <CardDescription className="line-clamp-4 text-sm">
-                        {certificate.description}
-                      </CardDescription>
-                    </CardContent>
-                  </Card>
-                </PinContainer>
-              </motion.div>
-            ))}
+           <Card className="h-full overflow-hidden group cursor-pointer transition-all duration-300 hover:shadow-lg hover:shadow-primary/25">
+              <CardContent className="p-0 h-full">
+                <div className="relative h-48 w-full">
+                  <Image 
+                    src={cert.image}
+                    alt="Certification Image"
+                    fill
+                    className="object-cover"
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  />
+                  <div className="absolute inset-0 flex flex-col items-center justify-center p-4 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                    <h1 className="text-white text-lg font-extra-bold">{cert.title}</h1>
+                    <p className="text-white text-center text-sm">{cert.description}</p>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
           </motion.div>
-        </motion.section>
+        ))}
+      </motion.div>
+    </motion.section>
 
         <motion.section
           className="flex flex-col gap-6 items-center justify-center mt-40"
--- src/app/static-data/cert.ts
+++ src/app/static-data/cert.ts
@@ -3,21 +3,31 @@ export const certifications= [
     {
       title: "CCNA Networking 1",
       link: "https://www.credly.com/badges/dc25122f-6542-4156-ba5b-6ca4b0b989cd/linked_in_profile",
-      description : "Given by Cisco. Certifies knowledge of networking including IP addressing, Ethernet, and the like."
+      description : "Given by Cisco. Certifies knowledge of networking including IP addressing, Ethernet, and the like.",
+      image : "/Cisco-Networking.png"
     },
     {
       title: "IT Specialist Python",
       link: "https://www.credly.com/badges/3395c826-8449-4710-8756-dd91ec998733/public_url",
-      description : "Issued by Pearson. Certifies proficiency and problem solving skills using Python."
+      description : "Issued by Pearson. Certifies proficiency and problem solving skills using Python.",
+      image : "/Pearson-Python.png"
     },
     {
       title: "GitHub Foundations",
       link: "https://www.credly.com/badges/9bd3f90d-6e7f-46b6-987b-a5d567956bb6/public_url",
-      description : "Bestwoed by GitHub. Certifies knowledge of the Git and GitHub."
+      description : "Bestwoed by GitHub. Certifies knowledge of the Git and GitHub.",
+      image : "/GitHub-Foundations.png"
     },
     {
       title: "Oracle Certified Professional-AI",
       link: "https://catalog-education.oracle.com/ords/certview/sharebadge?id=5902AED8C94E8FBB488F0BFE909F6088B1B562CFD0518B74F7A3F345354ED61D",
-      description : "Given by Oracle. Certifies knowledge of Machine Learning and Artificial Intelligence."
+      description : "Given by Oracle. Certifies knowledge of Machine Learning and Artificial Intelligence.",
+      image : "/Oracle-AI.png"
+    },
+    {
+      title: "2024 NASA Space Apps Global Nominee",
+      link: "https://www.spaceappschallenge.org/nasa-space-apps-2024/awards/global-nominees/",
+      description : "Given by NASA. Lead a team named THE POWER to develop an innovative solution for the 2024 NASA Space Apps Challenge and won as Global Nomimee and People's Choice.",
+      image : "/NASA-Logo.png"
     },
   ] as const;
