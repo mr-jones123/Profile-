b4174b6c6e4412846f0ef3c59becf8a669fdde98
Xynil Jhed Lacap <lacapxyniljhed@gmail.com>
2025-03-20 17:46:34 +0800

    chore: remove some ui for cleanliness

--- src/components/sanityBlogs.tsx
+++ src/components/sanityBlogs.tsx
@@ -41,7 +41,6 @@ export default function SanityBlogs(){
 
   return (
     <div className="max-w-7xl mx-auto py-12">
-      <h1 className="text-4xl md:text-5xl font-bold text-center mb-12">Blogs</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-4 md:px-6">
         {loading? <h1 className="text-2xl text-white">Loading Contents</h1> : queryResults?.map((post) => (
           <Link
